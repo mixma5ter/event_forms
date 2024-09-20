3aae1b71694fc429db1c8d9aa113313ed04e9a1c
mixmaster <deejay999@yandex.ru>
2024-09-20 15:55:31 +0300

    config added

--- frontend/event_forms_frontend/vite.config.js
+++ frontend/event_forms_frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "url"
+
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: true,
+    port: 8000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  }
 })
