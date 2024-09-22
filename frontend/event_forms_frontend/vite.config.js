45a0f1ad58b40b6305cdb7ac41139baf3997ed8a
mixmaster <deejay999@yandex.ru>
2024-09-22 08:54:31 +0300

    frontend: config setup

--- frontend/event_forms_frontend/vite.config.js
+++ frontend/event_forms_frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     server: {
         host: true,
-        port: 8080,
+        port: 5173,
         server: {
             proxy: {
                 '/api': {
